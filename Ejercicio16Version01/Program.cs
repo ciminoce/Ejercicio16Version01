3a1dd8d9aec4d0e4280cf1b2ec5010010ec3c3d9
ciminoce <ciminoce@yahoo.com.ar>
2021-05-31 20:09:31 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio16Version01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio16Version01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int unNumero;
+            Console.Write("Ingrese un número entero:");
+            unNumero = Convert.ToInt32(Console.ReadLine());
+            if (unNumero%2==0)
+            {
+                Console.WriteLine($"{unNumero} es par");
+            }
+            else
+            {
+                Console.WriteLine($"{unNumero} es impar");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
